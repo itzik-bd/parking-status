2c28f983db9fd0cf1f66b84dedbe7611a07a7691
Itzik Bar-David <itzik6@gmail.com>
2021-11-22 10:48:31 +0200

    ffmpeg layer automatic deployment

--- infrastructure/resource.video-stream-capture.tf
+++ infrastructure/resource.video-stream-capture.tf
@@ -13,11 +13,9 @@ resource "aws_lambda_function" "video-stream-capture" {
   handler           = "lambda.handler"
   runtime           = "python3.8"
   timeout           = 30
-
-  # this layer was manually deployed from:
-  # https://serverlessrepo.aws.amazon.com/applications/us-east-1/145266761615/ffmpeg-lambda-layer
-  # TODO: automate it via terraform
-  layers            = ["arn:aws:lambda:eu-central-1:814134525579:layer:ffmpeg:3"]
+  layers            = [
+    aws_serverlessapplicationrepository_cloudformation_stack.ffmpeg_layer.outputs["LayerVersion"]
+  ]
 
   environment {
     variables = {
@@ -25,4 +23,13 @@ resource "aws_lambda_function" "video-stream-capture" {
       CAMERA_ADDRESS = var.camera_address
     }
   }
+}
+
+# see https://serverlessrepo.aws.amazon.com/applications/us-east-1/145266761615/ffmpeg-lambda-layer
+resource "aws_serverlessapplicationrepository_cloudformation_stack" "ffmpeg_layer" {
+  name           = "${var.app_name}--${var.environment_name}--ffmpeg-lambda-layer"
+  application_id = "arn:aws:serverlessrepo:us-east-1:145266761615:applications/ffmpeg-lambda-layer"
+  capabilities = [
+    "CAPABILITY_IAM"
+  ]
 }
