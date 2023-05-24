4b850d0910e197f7ffc2ebaced170b930f32ec25
Itzik Bar-David <itzik6@gmail.com>
2023-05-24 22:04:28 +0300

    feat: small changes

--- infrastructure/reasource.ecr.tf
+++ infrastructure/reasource.ecr.tf
@@ -6,3 +6,27 @@ resource "aws_ecr_repository" "capture-analyzer-registry" {
     scan_on_push = false
   }
 }
+
+resource "aws_ecr_lifecycle_policy" "foopolicy" {
+  repository = aws_ecr_repository.capture-analyzer-registry.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Expire images older than 3 days",
+            "selection": {
+                "tagStatus": "untagged",
+                "countType": "sinceImagePushed",
+                "countUnit": "days",
+                "countNumber": 3
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
